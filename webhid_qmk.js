fa0300235213f06c82adb47f5a085e3e508b6307
Takuya Urakawa <urkwtky@gmail.com>
2020-03-12 01:50:04 +0900

    fix data size

--- webhid_qmk.js
+++ webhid_qmk.js
@@ -4,9 +4,20 @@ const sendButton = document.getElementById("send");
 const deviceFilter = [{ usagePage:0xFF60, usage: 0x61}];
 let device;
 
+function sendData() {
+	console.log("send to device");
+	let reportData = new Uint8Array(32);
+	for(let i=0;i<32;i++) {
+		reportData[i] = i+1;
+	}
+	device.sendReport(0, reportData);
+}
+
 connectButton.addEventListener("click", async ()=> {
 	console.log("start connecting");
 	try {
+		sendButton.disabled = "true";
+		sendButton.removeEventListener("click",sendData)
 		device = await navigator.hid.requestDevice({filters: deviceFilter});
 		console.log("found device!");
 		await device.open();
@@ -18,15 +29,11 @@ connectButton.addEventListener("click", async ()=> {
 			console.log("recieved from device", new Uint8Array(event.data.buffer));
 		});
 
-		sendButton.addEventListener("click", ()=> {
-			console.log("send to device");
-			let reportData = new Uint8Array([1,2,3,4,5,6,7,8]);
-			device.sendReport(0, reportData);
-		});
+		sendButton.addEventListener("click", sendData);
 
-		let reportData = new Uint8Array(8);
-		reportData[0] = 1;
-		device.sendReport(0, reportData);
+		// let reportData = new Uint8Array(32);
+		// reportData[0] = 1;
+		// device.sendReport(0, reportData);
 
 	} catch (error) {
 		console.warn("no device", error);
